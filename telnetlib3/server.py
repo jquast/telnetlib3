5ddae89ba362c3b135610a5ef37294a64bd381ee
jquast <contact@jeffquast.com>
2013-05-25 20:14:19 -0400

    make telnetlib.server default entry point

--- telnetlib3/server.py
+++ telnetlib3/server.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
 import collections
 import traceback
 import datetime
+import argparse
 import logging
 import codecs
 import shlex
@@ -685,4 +687,47 @@ class TelnetServer(tulip.protocols.Protocol):
         self.transport.close ()
         self._closing = True
 
+ARGS = argparse.ArgumentParser(description="Run simple telnet server.")
+ARGS.add_argument(
+    '--host', action="store", dest='host',
+    default='127.0.0.1', help='Host name')
+ARGS.add_argument(
+    '--port', action="store", dest='port',
+    default=6023, type=int, help='Port number')
+ARGS.add_argument(
+    '--loglevel', action="store", dest="loglevel",
+    default='info', type=str, help='Loglevel (debug,info)')
+
+def main():
+    import logging
+    import locale
+    args = ARGS.parse_args()
+    if ':' in args.host:
+        args.host, port = args.host.split(':', 1)
+        args.port = int(port)
+    locale.setlocale(locale.LC_ALL, '')
+    enc = locale.getpreferredencoding()
+    log = logging.getLogger()
+    log_const = args.loglevel.upper()
+    assert (log_const in dir(logging)
+            and isinstance(getattr(logging, log_const), int)
+            ), args.loglevel
+    log.setLevel(getattr(logging, log_const))
+    log.debug('default_encoding is {}'.format(enc))
+
+    loop = tulip.get_event_loop()
+    func = loop.start_serving(lambda: TelnetServer(default_encoding=enc),
+            args.host, args.port)
+
+    for sock in loop.run_until_complete(func):
+        # XXX --- we could set our socket OOBINLINE, and recieve IAC+DM as
+        # a normally interpreted signal, and find some way to go about
+        # ignoring it; but the only impl. I've found is BSD Client, which
+        # appears to lock up after 'send synch' --- XXX
+        #sock.setsockopt(socket.SOL_SOCKET, socket.SO_OOBINLINE, 1)
+        logging.info('Listening on %s', sock.getsockname())
+    loop.run_forever()
+
+if __name__ == '__main__':
+    main()
 
