ffee37578c2158ad71f92ca530e6e3a5ca2ad9ea
jquast <contact@jeffquast.com>
2013-12-27 02:22:41 -0800

    pep8 and tulip->asyncio, (works again..)

--- examples/honeypot.py
+++ examples/honeypot.py
@@ -3,28 +3,28 @@
 import argparse
 import logging
 import random
+import asyncio
 
-import telnetlib3
-from telnetlib3 import tulip
 from telnetlib3 import TelnetServer, Telsh, TelnetStream, TelnetShellStream
 
 #: A simple SLC tab that offers nearly all characters for negotiation,
 #  but has no default values of its own, soliciting them from client.
-from telnetlib3.slc import SLC_FORW1, SLC, SLC_NOSUPPORT, _POSIX_VDISABLE
-from telnetlib3.slc import SLC_FORW2, SLC_EOF, SLC_EL, SLC_ABORT, SLC_XOFF
-from telnetlib3.slc import SLC_RP, SLC_AO, SLC_AYT, SLC_SYNCH, SLC_EC, SLC_IP
-from telnetlib3.slc import SLC_XON, SLC_EW, SLC_LNEXT, SLC_SUSP, SLC_BRK
-from telnetlib3.slc import SLC_EOR
-SLC_TAB = { SLC_FORW1: SLC(SLC_NOSUPPORT, _POSIX_VDISABLE),
-            SLC_FORW2: SLC(SLC_NOSUPPORT, _POSIX_VDISABLE),
-            SLC_EOF: SLC(), SLC_EC: SLC(),
-            SLC_EL: SLC(), SLC_IP: SLC(),
-            SLC_ABORT: SLC(), SLC_XON: SLC(),
-            SLC_XOFF: SLC(), SLC_EW: SLC(),
-            SLC_RP: SLC(), SLC_LNEXT: SLC(),
-            SLC_AO: SLC(), SLC_SUSP: SLC(),
-            SLC_AYT: SLC(), SLC_BRK: SLC(),
-            SLC_SYNCH: SLC(), SLC_EOR: SLC(), }
+from telnetlib3.slc import (SLC_FORW1, SLC, SLC_NOSUPPORT, _POSIX_VDISABLE,
+                            SLC_FORW2, SLC_EOF, SLC_EL, SLC_ABORT, SLC_XOFF,
+                            SLC_RP, SLC_AO, SLC_AYT, SLC_SYNCH, SLC_EC, SLC_IP,
+                            SLC_XON, SLC_EW, SLC_LNEXT, SLC_SUSP, SLC_BRK,
+                            SLC_EOR)
+
+SLC_TAB = {SLC_FORW1: SLC(SLC_NOSUPPORT, _POSIX_VDISABLE),
+           SLC_FORW2: SLC(SLC_NOSUPPORT, _POSIX_VDISABLE),
+           SLC_EOF: SLC(), SLC_EC: SLC(),
+           SLC_EL: SLC(), SLC_IP: SLC(),
+           SLC_ABORT: SLC(), SLC_XON: SLC(),
+           SLC_XOFF: SLC(), SLC_EW: SLC(),
+           SLC_RP: SLC(), SLC_LNEXT: SLC(),
+           SLC_AO: SLC(), SLC_SUSP: SLC(),
+           SLC_AYT: SLC(), SLC_BRK: SLC(),
+           SLC_SYNCH: SLC(), SLC_EOR: SLC(), }
 
 # honeypot shell,
 
@@ -48,12 +48,13 @@ SLC_TAB = { SLC_FORW1: SLC(SLC_NOSUPPORT, _POSIX_VDISABLE),
 # but aware of the 4-stage context; command processing doesnt
 # begin until after AUTH
 
+
 class HoneyShell(Telsh):
     def __init__(self, server, stream=TelnetShellStream, log=logging):
         Telsh.__init__(self, server, stream, log)
         self.behaviors = [
-                NONAME, DDWRT, CWAV1, CAIR1, CAIR2, P661HNUF1, BUSYBOX,
-                NOLOGIN, NONENAME, LOCALHOST, LOCALHOSTLOCALDOMAIN, BADNAT, ]
+            NONAME, DDWRT, CWAV1, CAIR1, CAIR2, P661HNUF1, BUSYBOX,
+            NOLOGIN, NONENAME, LOCALHOST, LOCALHOSTLOCALDOMAIN, BADNAT, ]
         self.pick_behavior()
 
     def pick_behavior(self):
@@ -79,14 +80,16 @@ class HoneyShell(Telsh):
 # a modified variant of the basic TelnetStream that requests additional
 # ENV parameters
 
+
 class HoneyStream(TelnetStream):
     default_env_request = (
-            "USER HOSTNAME UID EUID TERM COLUMNS LINES DISPLAY SYSTEMTYPE "
-            "LOGNAME VISUAL EDITOR BASH_VERSION KSH_VERSION PWD OLDPWD "
-            "SSH_CONNECTION SSH_CLIENT SSH_TTY SSH_AUTH_SOCK SHELL "
-           "PS1 PS2 _ LANG SHELLOPTS BASHOPTS SFUTLNTVER SFUTLNTMODE "
-            "MACHTYPE HOSTTYPE LC_ALL HISTFILE TMPDIR SECONDS "
-            ).split()
+        "USER HOSTNAME UID EUID TERM COLUMNS LINES DISPLAY SYSTEMTYPE "
+        "LOGNAME VISUAL EDITOR BASH_VERSION KSH_VERSION PWD OLDPWD "
+        "SSH_CONNECTION SSH_CLIENT SSH_TTY SSH_AUTH_SOCK SHELL "
+        "PS1 PS2 _ LANG SHELLOPTS BASHOPTS SFUTLNTVER SFUTLNTMODE "
+        "MACHTYPE HOSTTYPE LC_ALL HISTFILE TMPDIR SECONDS "
+    ).split()
+
 
 # a modified telnet server, with 4-stage promotion process
 
@@ -104,12 +107,8 @@ class TelnetHoneypotServer(TelnetServer):
         """
         if self.server_fqdn.done():
             return 'Welcome to {} !\r\nlogin: '.format(
-                    self.server_fqdn.result())
+                self.server_fqdn.result())
         return ''
-
-    def begin_negotiation(self):
-        self.fast_edit = False
-        TelnetServer.begin_negotiation(self)
 
     def request_advanced_opts(self):
         from telnetlib3 import telopt
@@ -129,7 +128,7 @@ class TelnetHoneypotServer(TelnetServer):
             if bytes([opt]) not in (telopt.LOGOUT, telopt.TM, telopt.LINEMODE):
                 self.stream.iac(telopt.WILL, bytes([opt]))
 
-        telnetlib3.server.TelnetServer.request_advanced_opts(self)
+        TelnetServer.request_advanced_opts(self)
 
     def line_received(self, input, *args):
         self.log.debug('line_received: {!r}'.format(input))
@@ -147,60 +146,75 @@ class TelnetHoneypotServer(TelnetServer):
 
 # various behaviors found in the wild
 
+
 class SimplePass():
     def display_password(self):
         return '\r\nPassword: '
+
 
 class NoBanner():
     def display_banner(self):
         return ''
 
+
 class NONAME(SimplePass, NoBanner):
     def display_login(self):
         return 'login: '
+
 
 class DDWRT(SimplePass):
     def display_banner(self):
         return ('DD-WRT v24-sp2 std (c) 2011 NewMedia-NET GmbH\r\n'
                 'Release: 05/08/11 (SVN revision: 16994)\r\n')
+
     def display_login(self):
         return '\r\nDD-WRT login: '
+
 
 class CWAV1(SimplePass, NoBanner):
     def display_login(self):
         return 'CWAV-275v2TT login: '
 
+
 class CAIR1(SimplePass, NoBanner):
     def display_login(self):
         return 'CAir5452 login: '
+
 
 class CAIR2(SimplePass, NoBanner):
     def display_login(self):
         return 'CAir5341TT login: '
 
+
 class P661HNUF1(SimplePass, NoBanner):
     def display_login(self):
         return 'P-661HNU-F1 login: '
+
 
 class BUSYBOX(SimplePass, NoBanner):
     def display_login(self):
         return 'BusyBox on localhost login: '
 
+
 class NOLOGIN(SimplePass, NoBanner):
     def display_login(self):
         return self.display_password()
+
 
 class NONENAME(SimplePass, NoBanner):
     def display_login(self):
         return '(none) login: '
 
+
 class LOCALHOST(SimplePass, NoBanner):
     def display_login(self):
         return 'localhost login: '
 
+
 class LOCALHOSTLOCALDOMAIN(SimplePass, NoBanner):
     def display_login(self):
         return 'localhost.localdomain login: '
+
 
 class BADNAT(SimplePass, NoBanner):
     def display_login(self):
@@ -219,26 +233,35 @@ ARGS.add_argument(
     '--loglevel', action="store", dest="loglevel",
     default='info', type=str, help='Loglevel (debug,info)')
 
+
+def start_server(loop, log, host, port):
+    # create_server recieves a callable that returns a Protocol
+    # instance; wrap using `lambda' so that the specified logger
+    # instance (whose log-level is specified by cmd-line argument)
+    # may be used.
+    func = loop.create_server(
+        lambda: TelnetHoneypotServer(
+            stream=HoneyStream, shell=HoneyShell, encoding='ascii'),
+        host, port)
+    server = loop.run_until_complete(func)
+    log.info('Listening on %s', server.sockets[0].getsockname())
+
+
 def main():
-    import logging
     args = ARGS.parse_args()
     if ':' in args.host:
         args.host, port = args.host.split(':', 1)
         args.port = int(port)
-    log = logging.getLogger()
-    log_const = args.loglevel.upper()
-    assert (log_const in dir(logging)
-            and isinstance(getattr(logging, log_const), int)
-            ), args.loglevel
-    log.setLevel(getattr(logging, log_const))
 
-    loop = tulip.get_event_loop()
-    honey_server_factory = lambda: TelnetHoneypotServer(
-        stream=HoneyStream, shell=HoneyShell, encoding='ascii')
-    func = loop.start_serving(honey_server_factory, args.host, args.port)
+    # use generic 'logging' instance, and set the log-level as specified by
+    # command-line argument --loglevel
+    fmt = '%(asctime)s %(filename)s:%(lineno)d %(message)s'
+    logging.basicConfig(format=fmt)
+    log = logging.getLogger('telnet_server')
+    log.setLevel(getattr(logging, args.loglevel.upper()))
 
-    socks = loop.run_until_complete(func)
-    logging.info('Listening on %s', socks[0].getsockname())
+    loop = asyncio.get_event_loop()
+    start_server(loop, log, args.host, args.port)
     loop.run_forever()
 
 if __name__ == '__main__':
