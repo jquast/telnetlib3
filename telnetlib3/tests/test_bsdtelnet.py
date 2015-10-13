f0f14cf0af41b7739af1e1c5ed8a43bca95cc984
Jeff Quast <contact@jeffquast.com>
2015-10-13 16:36:54 -0700

    complete ipv4 passing, ipv6 fail, functional test
    requires release of pexpect to pass CI for first tass

--- telnetlib3/tests/test_bsdtelnet.py
+++ telnetlib3/tests/test_bsdtelnet.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Functionally tests telnetlib3 as a server, driving bsd telnet by pexpect!"""
 # std imports
+import functools
 import logging
 import asyncio
 import io
@@ -19,11 +20,9 @@ from pytest_asyncio.plugin import (
 
 @pytest.fixture
 def log():
-    log = logging.getLogger(__name__)
-    fmt = '%(levelname)s %(filename)s:%(lineno)d %(message)s'
-    logging.basicConfig(format=fmt)
-    log.setLevel(logging.DEBUG)
-    return log
+    _log = logging.getLogger(__name__)
+    _log.setLevel(logging.DEBUG)
+    return _log
 
 
 @pytest.fixture(scope="module", params=["0.0.0.0", "::1"])
@@ -32,25 +31,67 @@ def bind_host(request):
 
 
 @pytest.mark.asyncio
-def test_bsdtelnet(event_loop, bind_host, unused_tcp_port, log):
-    func = event_loop.create_server(lambda: telnetlib3.TelnetServer(log=log),
-                                    bind_host, unused_tcp_port)
+def test_bsdtelnet(caplog, event_loop, bind_host, unused_tcp_port, log):
 
-    server = event_loop.run_until_complete(func)
+    event_loop.set_debug(True)
+    caplog.setLevel(logging.DEBUG)
 
-    log.info('Listening on %s', server.sockets[0].getsockname())
+    waiter_server_connected = asyncio.Future()
+    waiter_server_telopt = asyncio.Future()
+    waiter_server_encoding = asyncio.Future()
+    waiter_server_closed = asyncio.Future()
 
-    pexpect_log = io.StringIO()
+    server = yield from event_loop.create_server(
+        protocol_factory=lambda: telnetlib3.TelnetServer(
+            waiter_connected=waiter_server_connected,
+            waiter_closed=waiter_server_closed,
+            waiter_telopt=waiter_server_telopt,
+            waiter_encoding=waiter_server_encoding,
+            log=log),
+        host=bind_host, port=unused_tcp_port)
 
-    child = pexpect.spawnu(command='telnet', timeout=5,
-                           logfile=pexpect_log)
+    log.info('Listening on {host} {port}'.format(
+             host=server.sockets[0].getsockname(),
+             port=unused_tcp_port))
 
-    child.sendline("status")
-    yield from child.expect_exact("No connection.", async=True)
+    child = pexpect.spawn(
+        command='telnet', timeout=1,
+        encoding='utf8', echo=False)
+
+    # patch pexpect to debug-copy i/o to separate streams
+    child.delaybeforesend = 0.0
+    child.logfile_read = io.StringIO()
+    child.logfile_send = io.StringIO()
+
+    telnet_options = ('options',)
+    # telnet_options = ('options', 'debug', 'netdata', 'prettydump',)
+    # and to enable all telnet client options
+    for opt in telnet_options:
+        yield from child.expect("telnet> ", async=True)
+        child.sendline(u"set {0}".format(opt))
+
+    # before connecting,
+    yield from child.expect(u"telnet> ", async=True)
+    child.sendline(u"open {0} {1}".format(bind_host, unused_tcp_port))
     yield from child.expect_exact("Escape character is '^]'.", async=True)
-    yield from child.expect_exact('telnet>', async=True)
-    child.sendline("open {0} {1}".format(bind_host, unused_tcp_port))
 
-#    val = yield from server.connected
-#    assert val
-#    assert child.read()
+    # now await all completed negotiations
+    child_expect = functools.partial(child.expect_exact,
+                                     timeout=None,
+                                     async=True)
+
+    done, pending = yield from asyncio.wait(
+        [child_expect('SENT WILL BINARY'), waiter_server_encoding],
+        loop=event_loop, timeout=2,
+        return_when=asyncio.ALL_COMPLETED)
+
+    # inquire server of our known status and exit.
+    child.sendline(u"quit")
+
+    done, pending = yield from asyncio.wait(
+        [child_expect('Connection closed by foreign host.'),
+         waiter_server_closed],
+        loop=event_loop, timeout=1,
+        return_when=asyncio.ALL_COMPLETED)
+
+    assert not pending
