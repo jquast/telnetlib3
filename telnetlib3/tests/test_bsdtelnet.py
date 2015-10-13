de87a440b8c0ec3ecd5779d03b712f3c32c58454
Jeff Quast <contact@jeffquast.com>
2015-10-12 20:19:14 -0700

    add test, difficulties with using 'yield from'
    once I add 'yield from' (commented section), the test
    errors out in a completely unexpected way, describing
    that all position arguments are unment. I believe I

---
+++ telnetlib3/tests/test_bsdtelnet.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""Functionally tests telnetlib3 as a server, driving bsd telnet by pexpect!"""
+# std imports
+import logging
+import asyncio
+import io
+
+# local imports
+import telnetlib3
+
+# 3rd party imports
+import pexpect
+import pytest
+from pytest_asyncio.plugin import (
+    unused_tcp_port,
+    event_loop,
+)
+
+
+@pytest.fixture
+def log():
+    log = logging.getLogger(__name__)
+    fmt = '%(levelname)s %(filename)s:%(lineno)d %(message)s'
+    logging.basicConfig(format=fmt)
+    log.setLevel(logging.DEBUG)
+    return log
+
+
+@pytest.fixture(scope="module", params=["0.0.0.0", "::1"])
+def bind_host(request):
+    return request.param
+
+
+def test_bsdtelnet(event_loop, bind_host, unused_tcp_port, log):
+    func = event_loop.create_server(lambda: telnetlib3.TelnetServer(log=log),
+                                    bind_host, unused_tcp_port)
+
+    server = event_loop.run_until_complete(func)
+
+    log.info('Listening on %s', server.sockets[0].getsockname())
+
+    pexpect_log = io.StringIO()
+
+    child = pexpect.spawn(command='telnet', timeout=5,
+                          encoding='utf-8', logfile=pexpect_log)
+
+    child.sendline("status")
+#    yield from child.expect_exact("No connection.", async=True)
+#    yield from child.expect_exact("Escape character is '^]'.", async=True)
+#    yield from child.expect_exact('telnet>', async=True)
