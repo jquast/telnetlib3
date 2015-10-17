fd02b7803208ca987d4fee4f7ff9764b966b2e0e
Jeff Quast <contact@jeffquast.com>
2015-10-17 12:07:25 -0700

    Workaround nc(1) in FreeBSD

--- telnetlib3/tests/test_netcat.py
+++ telnetlib3/tests/test_netcat.py
@@ -1,6 +1,7 @@
 """Functionally tests telnetlib3 as a server using nc(1)."""
 # std imports
 import subprocess
+import platform
 import asyncio
 import math
 import time
@@ -20,10 +21,12 @@ import pexpect
 
 
 def get_netcat():
+    """ Find and return best-matching IPv6 capable, OpenBSD-derived nc(1)."""
     netcat_paths=('nc',
                   'netcat',
                   '/usr/bin/nc',
                   '/usr/local/bin/nc',
+                  # debian,
                   '/bin/nc.openbsd')
     for nc_name in netcat_paths:
         prog = pexpect.which(nc_name)
@@ -47,12 +50,15 @@ def get_netcat():
                     reason="Requires IPv6 capable (OpenBSD-borne) nc(1)")
 @pytest.mark.asyncio
 def test_netcat_z(event_loop, bind_host, unused_tcp_port, log):
+    """
+    Using nc(1) -z, ensure server behaves well against "port scanning".
+    """
 
-    waiter_closed = asyncio.Future()
+    connection_closed = asyncio.Future()
 
     server = yield from event_loop.create_server(
         protocol_factory=lambda: TestTelnetServer(
-            waiter_closed=waiter_closed,
+            waiter_closed=connection_closed,
             log=log),
         host=bind_host, port=unused_tcp_port)
 
@@ -64,22 +70,26 @@ def test_netcat_z(event_loop, bind_host, unused_tcp_port, log):
     )
 
     done, pending = yield from asyncio.wait(
-        [waiter_closed, netcat.wait()],
+        [connection_closed, netcat.wait()],
         loop=event_loop, timeout=1)
 
-    assert not pending, (netcat, waiter_closed)
+    assert not pending, (netcat, connection_closed)
 
 
 @pytest.mark.skipif(get_netcat() is None,
                     reason="Requires IPv6 capable (OpenBSD-borne) nc(1)")
+@pytest.mark.skipif(platform.system() == "FreeBSD",
+                    reason="FreeBSD nc(1) does not exit on socket close.")
 @pytest.mark.asyncio
-def test_netcat_z_timeout(event_loop, bind_host, unused_tcp_port, log):
-
-    waiter_closed = asyncio.Future()
+def test_netcat_t_timeout_1s(event_loop, bind_host, unused_tcp_port, log):
+    """
+    Using nc(1) -t, instruct server to disconnect us by 1s. idle.
+    """
+    connection_closed = asyncio.Future()
 
     server = yield from event_loop.create_server(
         protocol_factory=lambda: TestTelnetServer(
-            waiter_closed=waiter_closed,
+            waiter_closed=connection_closed,
             log=log),
         host=bind_host, port=unused_tcp_port)
 
@@ -92,17 +102,28 @@ def test_netcat_z_timeout(event_loop, bind_host, unused_tcp_port, log):
         stderr=subprocess.PIPE
     )
 
-    # set idle period to 1s.
-    netcat.stdin.write(u'set TIMEOUT=1\r'.encode('ascii'))
-    yield from netcat.stdin.drain()
+    # expect a prompt or some such.
+    yield from netcat.stdout.readline()
+
+    # begin timer,
+    log.debug('begin timer')
+
     stime = time.time()
 
+    netcat.stdin.write(b'set TIMEOUT=1\r')
+
+    # expect disconnection
     done, pending = yield from asyncio.wait(
-        [waiter_closed, netcat.wait()],
-        loop=event_loop, timeout=3)
+        [connection_closed, netcat.wait()],
+        loop=event_loop, timeout=2.1)
+
     duration = time.time() - stime
 
-    assert len(pending) == 0, (waiter_closed, netcat)
+    log.debug('done: {0}'.format(done))
+    log.debug('pending: {0}'.format(pending))
+    log.debug('duration: {0}'.format(duration))
 
-    # we were disconnected after idling for ~1 second.
+    assert 0 == len(pending)
+
+    # we were disconnected after idling for >=~1.0000 second.
     assert math.floor(duration) == 1
