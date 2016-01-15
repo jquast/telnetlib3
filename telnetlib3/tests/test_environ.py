a55055e8bc9a7481596263686836d6ae3f9337d1
Jeff Quast <contact@jeffquast.com>
2016-01-15 14:26:37 -0800

    test coverage for various environ callbacks

--- telnetlib3/tests/test_environ.py
+++ telnetlib3/tests/test_environ.py
@@ -58,27 +58,73 @@ def test_telnet_server_on_environ(
 
 
 @pytest.mark.asyncio
-def test_telnet_client_tty_cmdline(bind_host, unused_tcp_port,
-                                   event_loop, log):
-    """Test executing telnetlib3/client.py as client using a tty (pexpect)"""
-    # this code may be reduced when pexpect asyncio is bugfixed ..
-    # we especially need pexpect to pass sys.stdin.isatty() test.
-    import os
-    import pexpect
-    prog, args = 'telnetlib3-client', [
-        bind_host, str(unused_tcp_port), '--loglevel=warn',]
+def test_telnet_client_send_environ(event_loop, bind_host,
+                                    unused_tcp_port, log):
+    """Test Client's callback method send_environ() for specific requests."""
+    # given
+    _waiter = asyncio.Future()
+    given_cols = 19
+    given_rows = 84
+    given_encoding = 'cp437'
+    given_term = 'vt220'
 
-    class HelloServer(asyncio.Protocol):
-        def connection_made(self, transport):
-            super().connection_made(transport)
-            transport.write(b'hello, space cadet.\r\n')
-            # hangup
-            event_loop.call_later(0.2, transport.close)
+    class ServerTestEnviron(telnetlib3.TelnetServer):
+        def on_environ(self, mapping):
+            super().on_environ(mapping)
+            _waiter.set_result(mapping)
 
-    # start vanilla tcp server
-    yield from event_loop.create_server(HelloServer,
-                                        bind_host, unused_tcp_port)
-    import sys
-    proc = pexpect.spawn(prog, args)
-    yield from proc.expect(pexpect.EOF, async=True, timeout=5)
-    assert proc.before.splitlines()[0] == b'hello, space cadet.'
+    yield from telnetlib3.create_server(
+        protocol_factory=ServerTestEnviron,
+        host=bind_host, port=unused_tcp_port,
+        loop=event_loop, log=log)
+
+    reader, writer = yield from telnetlib3.open_connection(
+        host=bind_host, port=unused_tcp_port, loop=event_loop,
+        cols=given_cols, rows=given_rows, encoding=given_encoding,
+        term=given_term)
+
+    mapping = yield from asyncio.wait_for(_waiter, 0.5)
+    assert mapping == {
+        'COLUMNS': str(given_cols),
+        'LANG': 'en_US.' + given_encoding,
+        'LINES': str(given_rows),
+        'TERM': 'vt220'
+    }
+
+
+@pytest.mark.asyncio
+def test_telnet_server_reject_environ(event_loop, bind_host,
+                                    unused_tcp_port, log):
+    """Test Client's callback method send_environ() for specific requests."""
+    from telnetlib3.telopt import SB, NEW_ENVIRON
+    # given
+    _waiter = asyncio.Future()
+    given_cols = 19
+    given_rows = 84
+    given_encoding = 'cp437'
+    given_term = 'vt220'
+
+    class ServerTestEnviron(telnetlib3.TelnetServer):
+        def on_environ(self, mapping):
+            super().on_environ(mapping)
+            _waiter.set_result(mapping)
+
+        def on_request_environ(self):
+            return None
+
+    yield from telnetlib3.create_server(
+        protocol_factory=ServerTestEnviron,
+        host=bind_host, port=unused_tcp_port,
+        loop=event_loop, log=log)
+
+    reader, writer = yield from telnetlib3.open_connection(
+        host=bind_host, port=unused_tcp_port, loop=event_loop,
+        cols=given_cols, rows=given_rows, encoding=given_encoding,
+        term=given_term)
+
+    # this causes the client to expect the server to have demanded environment
+    # values, since it did, of course demand DO NEW_ENVIRON! However, our API
+    # choice here has chosen not to -- the client then indicates this as a
+    # failed sub-negotiation (SB + NEW_ENVIRON).
+    _failed = {key: val for key, val in writer.pending_option.items() if val}
+    assert _failed == {SB + NEW_ENVIRON: True}
