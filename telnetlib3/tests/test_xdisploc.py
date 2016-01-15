93a3a21151a8eb07aae16c1413724a54a188be6c
Jeff Quast <contact@jeffquast.com>
2016-01-15 14:26:00 -0800

    client-side xdisploc coverage

--- telnetlib3/tests/test_xdisploc.py
+++ telnetlib3/tests/test_xdisploc.py
@@ -49,3 +49,33 @@ def test_telnet_server_on_xdisploc(
     # verify,
     srv_instance = yield from asyncio.wait_for(_waiter, 0.5)
     assert srv_instance.get_extra_info('xdisploc') == 'alpha:0'
+
+
+@pytest.mark.asyncio
+def test_telnet_client_send_xdisploc(event_loop, bind_host, unused_tcp_port, log):
+    """Test Client's callback method send_xdisploc()."""
+    # given
+    _waiter = asyncio.Future()
+    given_xdisploc = 'alpha'
+
+    class ServerTestXdisploc(telnetlib3.TelnetServer):
+        def on_xdisploc(self, xdisploc):
+            super().on_xdisploc(xdisploc)
+            _waiter.set_result(xdisploc)
+
+        def begin_advanced_negotiation(self):
+            from telnetlib3.telopt import DO, XDISPLOC
+            super().begin_advanced_negotiation()
+            self.writer.iac(DO, XDISPLOC)
+
+    yield from telnetlib3.create_server(
+        protocol_factory=ServerTestXdisploc,
+        host=bind_host, port=unused_tcp_port,
+        loop=event_loop, log=log)
+
+    reader, writer = yield from telnetlib3.open_connection(
+        host=bind_host, port=unused_tcp_port, loop=event_loop,
+        xdisploc=given_xdisploc)
+
+    recv_xdisploc = yield from asyncio.wait_for(_waiter, 0.5)
+    assert recv_xdisploc == given_xdisploc
