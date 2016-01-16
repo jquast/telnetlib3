ba902528bc4d73ce81b01392090e14f3acfd7705
Jeff Quast <contact@jeffquast.com>
2016-01-15 19:29:41 -0800

    expose TelnetReader/TelnetWriter API

--- telnetlib3/__init__.py
+++ telnetlib3/__init__.py
@@ -3,6 +3,8 @@
 from .server_base import *
 from .server_shell import *
 from .server import *
+from .stream_writer import *
+from .stream_reader import *
 from .client_base import *
 from .client_shell import *
 from .client import *
@@ -18,6 +20,8 @@ __all__ = (
     client_shell.__all__ +
     client.__all__ +
 
+    stream_writer.__all__ +
+    stream_reader.__all__ +
     telopt.__all__ +
     slc.__all__
 )
