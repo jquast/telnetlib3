e5c5ed992449a8e98f0454a4410e658c5836784b
Jeff Quast <contact@jeffquast.com>
2020-09-13 11:47:35 -0400

    remove sphinx 'app.info', doesn't work any longer
    We don't pin sphinx versions, I guess this doesn't work anymore

--- docs/sphinxext/github.py
+++ docs/sphinxext/github.py
@@ -146,7 +146,7 @@ def setup(app):
     
     :param app: Sphinx application context.
     """
-    app.info('Initializing GitHub plugin')
+    #app.info('Initializing GitHub plugin')
     app.add_role('ghissue', ghissue_role)
     app.add_role('ghpull', ghissue_role)
     app.add_role('ghuser', ghuser_role)
